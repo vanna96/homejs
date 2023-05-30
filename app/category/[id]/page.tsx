6fa87236fb246680f9bb79e5fc1dff761d415f67
vanna96 <sovannapoung@gmail.com>
2023-05-30 16:55:52 +0700

    add cart ui, favorite, category detail, new coming, best selling

--- app/category/[id]/page.tsx
+++ app/category/[id]/page.tsx
@@ -1,125 +1,126 @@
 "use client";
 
-import { ArrowLeftOutlined, ShareAltOutlined } from "@ant-design/icons";
+import { GridItem } from "@/components/UI/GridItem";
+import { ListItem } from "@/components/UI/ListItem";
+import {
+  AppstoreOutlined,
+  BarsOutlined,
+  LeftOutlined,
+} from "@ant-design/icons";
+import { Affix, Segmented } from "antd";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function ItemDetail() {
-    const router = useRouter();
-    const item = {
-        id: 5,
-        name: "បាយឆាក្តាមយ៉ាងចូវ",
-        currency: "$",
-        imgUrl: "https://images-prod.healthline.com/hlcmsresource/images/AN_images/orange-juice-1296x728-feature.jpg",
-        price: "3.00",
-        promotion: "2.50",
-        badge: "20% OFF",
-        rate: "4.2",
-        details: [
-            {
-                label: "Select An Option",
-                type: "Radio",
-                min: 1,
-                max: 2,
-                required: true,
-                isOption: true,
-                options: [
-                    {
-                        id: 1,
-                        price: "1.80",
-                        name: "កែវតូច"
-                    },
-                    {
-                        id: 2,
-                        price: "2.00",
-                        name: "កែវកណ្តាល"
-                    },
-                    {
-                        id: 3,
-                        price: "2.20",
-                        name: "កែវធំ"
-                    }
-                ]
-            },
-            {
-                label: "ជម្រើសមុខម្ហូបបន្ថែម",
-                type: "check",
-                min: 1,
-                max: 2,
-                required: false,
-                options: [
-                    {
-                        id: 4,
-                        price: '0.00',
-                        name: "ពងទាចៀន"
-                    },
-                    {
-                        id: 5,
-                        price: "1.72",
-                        name: "ពងទាក្រឡុកសាច់ក្តាម"
-                    },
-                    {
-                        id: 6,
-                        price: "1.56",
-                        name: "ស្លាបមាន់បំពង"
-                    },
-                    {
-                        id: 7,
-                        price: "1.35",
-                        name: "ស៊ុបប្រហិតសាច់គោ"
-                    },
-                    {
-                        id: 8,
-                        price: "1.27",
-                        name: "ទឹកផាសិនស្រស់"
-                    }
-                ]
-            },
-            {
-                label: "ទឹកស៊ុបធ្វើពីសាច់ជ្រូក និងបន្លែ",
-                type: "Radio",
-                min: 1,
-                max: 2,
-                required: false,
-                options: [
-                    {
-                        id: 4,
-                        price: '0.15',
-                        name: "ស៊ុបថែមឆ្នឹងបន្លែ"
-                    },
-                    {
-                        id: 5,
-                        price: "0.00",
-                        name: "យកទឹកស៊ុប"
-                    },
-                    {
-                        id: 6,
-                        price: "0.00",
-                        name: "អត់យកស៊ុប"
-                    }
-                ]
-            }
-        ]
-    };
+  const router = useRouter();
+  const [listType, setListType] = useState("Grid");
+  const category = {
+    id: 1,
+    name: "Drink & Dessert",
+    imageUrl:
+      "https://s3-ap-southeast-1.amazonaws.com/foodcourt-production-bucket/upload/category/1681699808.jpg",
+  };
 
-    return <div className="max-w-md mx-auto containerStyle min-h-[100vh]">
-        <div className="ohlala">
-            <div style={{
-                backgroundImage: `url(${item?.imgUrl})`,
-                width: '100%',
-                height: '250px',
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover',
-            }} />
-            <div className='flex relative justify-start px-2'>
-                <div className="stickyIcon" onClick={() => router.back()}>
-                    <ArrowLeftOutlined style={{ color: '#db2877' }} />
-                </div>
-            </div>
-            <div className='flex relative justify-end px-2'>
-                <div className="stickyIcon">
-                    <ShareAltOutlined style={{ color: '#db2877' }} />
-                </div>
-            </div>
+  const items = [
+    {
+      id: 1,
+      name: "Pizza pie",
+      imgUrl:
+        "https://images.immediate.co.uk/production/volatile/sites/30/2020/10/Pizza-pie-546a53b.jpg?resize=768,574",
+      price: "$6.25",
+    },
+    {
+      id: 2,
+      name: "Spaghetti with Meat Sauce",
+      imgUrl:
+        "https://www.inspiredtaste.net/wp-content/uploads/2019/03/Spaghetti-with-Meat-Sauce-Recipe-1-1200.jpg",
+      price: "$8.25",
+    },
+    {
+      id: 3,
+      name: "French Fried Potatoes",
+      imgUrl:
+        "https://www.allrecipes.com/thmb/-Qlhx70a3HwawPd51kSIiGj-hpE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/4488416-0627cab55d4e44ec80f974fbc67befb7.jpg",
+      price: "$4.25",
+      rate: "4.2",
+    },
+    {
+      id: 4,
+      name: "Salad",
+      imgUrl:
+        "https://images.immediate.co.uk/production/volatile/sites/30/2014/05/Epic-summer-salad-hub-2646e6e.jpg",
+      price: "$3.25",
+      badge: "Free delivery",
+    },
+    {
+      id: 5,
+      name: "Orange Juice",
+      imgUrl:
+        "https://images-prod.healthline.com/hlcmsresource/images/AN_images/orange-juice-1296x728-feature.jpg",
+      price: "$3.00",
+      promotion: "$2.50",
+      badge: "20% OFF",
+    },
+  ];
+
+  return (
+    <div className="max-w-md mx-auto containerStyle min-h-[100vh]">
+      <title>Category Detail</title>
+      <div className="flex items-center justify-between bg-pink-600 h-[50px] text-white px-2">
+        <LeftOutlined
+          className="cursor-pointer font-semibold text-white"
+          onClick={() => router.back()}
+        />
+        <div
+          className="text-white font-light text-xl"
+          style={{ fontFamily: "__Roboto_Mono_65f3ca" }}
+        >
+          Category Detail
         </div>
+        <div>{""}</div>
+      </div>
+      <div
+        style={{
+          backgroundImage: `url(${category?.imageUrl})`,
+          width: "100%",
+          height: "200px",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          marginBottom: "30px",
+        }}
+      />
+      <div className="mx-4">
+        <Affix offsetTop={0}>
+          <div className="flex items-center justify-between mb-7 bg-white">
+            <h3
+              className="font-bold"
+              style={{ fontFamily: "__Roboto_Mono_65f3ca" }}
+            >
+              {category?.name}
+            </h3>
+            <Segmented
+              size="large"
+              value={listType}
+              onChange={(value: any) => setListType(value)}
+              options={[
+                {
+                  value: "List",
+                  icon: <BarsOutlined />,
+                },
+                {
+                  value: "Grid",
+                  icon: <AppstoreOutlined />,
+                },
+              ]}
+            />
+          </div>
+        </Affix>
+        {listType === "Grid" ? (
+          <GridItem items={items} />
+        ) : (
+          <ListItem items={items} />
+        )}
+      </div>
     </div>
+  );
 }
--- app/item/new-coming/page.tsx
+++ app/item/new-coming/page.tsx
@@ -1,3 +1,110 @@
+"use client";
+
+import { GridItem } from "@/components/UI/GridItem";
+import { ListItem } from "@/components/UI/ListItem";
+import {
+  AppstoreOutlined,
+  BarsOutlined,
+  LeftOutlined,
+} from "@ant-design/icons";
+import { Affix, Segmented } from "antd";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export default function newComming() {
-    return <>New Comming</>
+  const router = useRouter();
+  const [listType, setListType] = useState("Grid");
+
+  const items = [
+    {
+      id: 1,
+      name: "Pizza pie",
+      imgUrl:
+        "https://images.immediate.co.uk/production/volatile/sites/30/2020/10/Pizza-pie-546a53b.jpg?resize=768,574",
+      price: "$6.25",
+    },
+    {
+      id: 2,
+      name: "Spaghetti with Meat Sauce",
+      imgUrl:
+        "https://www.inspiredtaste.net/wp-content/uploads/2019/03/Spaghetti-with-Meat-Sauce-Recipe-1-1200.jpg",
+      price: "$8.25",
+    },
+    {
+      id: 3,
+      name: "French Fried Potatoes",
+      imgUrl:
+        "https://www.allrecipes.com/thmb/-Qlhx70a3HwawPd51kSIiGj-hpE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/4488416-0627cab55d4e44ec80f974fbc67befb7.jpg",
+      price: "$4.25",
+      rate: "4.2",
+    },
+    {
+      id: 4,
+      name: "Salad",
+      imgUrl:
+        "https://images.immediate.co.uk/production/volatile/sites/30/2014/05/Epic-summer-salad-hub-2646e6e.jpg",
+      price: "$3.25",
+      badge: "Free delivery",
+    },
+    {
+      id: 5,
+      name: "Orange Juice",
+      imgUrl:
+        "https://images-prod.healthline.com/hlcmsresource/images/AN_images/orange-juice-1296x728-feature.jpg",
+      price: "$3.00",
+      promotion: "$2.50",
+      badge: "20% OFF",
+    },
+  ];
+
+  return (
+    <div className="max-w-md mx-auto containerStyle min-h-[100vh]">
+      <title>New Coming</title>
+      <div className="flex items-center justify-between bg-pink-600 h-[50px] text-white px-2">
+        <LeftOutlined
+          className="cursor-pointer font-semibold text-white"
+          onClick={() => router.back()}
+        />
+        <div
+          className="text-white font-light text-xl"
+          style={{ fontFamily: "__Roboto_Mono_65f3ca" }}
+        >
+          New Coming
+        </div>
+        <div>{""}</div>
+      </div>
+      <div className="mx-4">
+        <Affix offsetTop={0}>
+          <div className="flex items-center justify-between mb-7 bg-white">
+            <h3
+              className="font-bold"
+              style={{ fontFamily: "__Roboto_Mono_65f3ca" }}
+            >
+              Lists
+            </h3>
+            <Segmented
+              size="large"
+              value={listType}
+              onChange={(value: any) => setListType(value)}
+              options={[
+                {
+                  value: "List",
+                  icon: <BarsOutlined />,
+                },
+                {
+                  value: "Grid",
+                  icon: <AppstoreOutlined />,
+                },
+              ]}
+            />
+          </div>
+        </Affix>
+        {listType === "Grid" ? (
+          <GridItem items={items} />
+        ) : (
+          <ListItem items={items} />
+        )}
+      </div>
+    </div>
+  );
 }
